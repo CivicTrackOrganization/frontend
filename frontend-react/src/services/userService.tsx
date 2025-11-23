92dd25dea9eac69d1259c66d930106934be59eff
Szymon <szymongryz@interia.pl>
2025-11-23 22:23:08 +0100

    remove unnecessary log

--- frontend-react/src/services/userService.tsx
+++ frontend-react/src/services/userService.tsx
@@ -14,9 +14,6 @@ export interface SignUpResponse {
 const API_URL = "http://localhost:8000";
 
 export const signUp = async (data: SignUpRequest): Promise<SignUpResponse> => {
-  console.log(
-    "data being send: " + data.email + " " + data.password + " " + data.username
-  );
   const response = await axios.post<SignUpResponse>(
     `${API_URL}/auth/sign-up/`,
     data,
